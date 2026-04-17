e18c1142554bcdb13996fb7cf06bc0cbd5e577ee
Mounesh Pattar <morphai.startup@gmail.com>
2026-04-17 21:49:55 +0530

    added zip download feature

--- generator/views.py
+++ generator/views.py
@@ -1,15 +1,42 @@
+import zipfile
+import io
+from django.http import HttpResponse
 from django.shortcuts import render
-from .services.code_generator import generate_project
 
 def home(request):
     if request.method == "POST":
         tech = request.POST.get("tech")
-        project_type = request.POST.get("type")
         topic = request.POST.get("topic")
-        difficulty = request.POST.get("difficulty")
 
-        result = generate_project(tech, project_type, topic, difficulty)
+        # Create ZIP in memory
+        buffer = io.BytesIO()
+        zip_file = zipfile.ZipFile(buffer, 'w')
 
-        return render(request, "index.html", {"result": result})
+        if tech == "django":
+            # Add files to ZIP
+            zip_file.writestr("project/manage.py", "# Django manage file")
+            zip_file.writestr("project/app/views.py", f"""
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to {topic}")
+""")
+
+        elif tech == "android":
+            zip_file.writestr("project/MainActivity.java", f"""
+public class MainActivity {{
+    protected void onCreate() {{
+        System.out.println("{topic}");
+    }}
+}}
+""")
+
+        zip_file.close()
+
+        # Send ZIP to user
+        response = HttpResponse(buffer.getvalue(), content_type='application/zip')
+        response['Content-Disposition'] = f'attachment; filename={topic}.zip'
+
+        return response
 
     return render(request, "index.html")
