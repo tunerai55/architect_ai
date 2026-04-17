07a09d1e77447248ee69f542c3b19e64e75347b8
Mounesh Pattar <morphai.startup@gmail.com>
2026-04-17 22:08:12 +0530

    final django project generator

--- generator/views.py
+++ generator/views.py
@@ -32,14 +32,82 @@ Welcome to {topic}
             buffer = io.BytesIO()
             zip_file = zipfile.ZipFile(buffer, 'w')
 
-            if tech == "django":
-                zip_file.writestr("project/manage.py", "# Django manage file")
+            zip_file.writestr("project/manage.py", """#!/usr/bin/env python
+import os
+import sys
 
-                zip_file.writestr("project/app/views.py", f"""
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
+""")
+
+zip_file.writestr("project/project/__init__.py", "")
+
+zip_file.writestr("project/project/settings.py", f"""
+SECRET_KEY = 'dummykey'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = []
+
+ROOT_URLCONF = 'project.urls'
+
+TEMPLATES = [
+    {{
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+    }},
+]
+
+WSGI_APPLICATION = 'project.wsgi.application'
+
+DATABASES = {{
+    'default': {{
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }}
+}}
+""")
+
+zip_file.writestr("project/project/urls.py", f"""
+from django.urls import path
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Welcome to {topic}")
+
+urlpatterns = [
+    path('', home),
+]
+""")
+
+zip_file.writestr("project/project/wsgi.py", """
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+application = get_wsgi_application()
+""")
+
+zip_file.writestr("project/requirements.txt", "Django")
+
+zip_file.writestr("project/README.md", f"""
+# {topic}
+
+## Run this project:
+
+1. pip install -r requirements.txt
+2. python manage.py runserver
+3. Open http://127.0.0.1:8000/
 """)
 
                 zip_file.writestr("project/README.md", f"# {topic}\nDjango Project")
