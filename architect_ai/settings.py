240b15b0ab9e3fb0fd12d0579b02fb9e1c9d1484
Mounesh Pattar <morphai.startup@gmail.com>
2026-04-24 08:25:23 +0530

    updated settings.py

--- architect_ai/settings.py
+++ architect_ai/settings.py
@@ -112,7 +112,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
@@ -121,3 +120,5 @@ STATIC_URL = "static/"
 import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'generate', 'templates')]
